07e11e5501d60e3d0b19187eb6606c7e9d8fa0f4
EC2 Default User <ec2-user@ip-172-31-10-57.ap-northeast-1.compute.internal>
2022-07-08 03:02:41 +0000

    [Finish]application-oyo-6

---
+++ app/models/book.rb
@@ -0,0 +1,13 @@
+class Book < ApplicationRecord
+  belongs_to :user
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_one_attached :profile_image
+  validates :title,presence:true
+  validates :body,presence:true,length:{maximum:200}
+   
+  def favorite_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+   
+end
